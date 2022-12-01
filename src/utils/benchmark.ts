e1c812ec88692837c235e55a0c13a4f44e2932dd
Wojciech Tałałaj <wojtal.pl@gmail.com>
2022-12-01 22:59:06 +0100

    Added heap usage

--- src/utils/benchmark.ts
+++ src/utils/benchmark.ts
@@ -4,27 +4,32 @@ export type Task<T> = () => T;
 
 export type Snapshot = {
   time: bigint;
-  memory: number;
+  rss: number;
+  heap: number;
 };
 
 const snapshot = (): Snapshot => ({
   time: hrtime.bigint(),
-  memory: process.memoryUsage().rss,
+  rss: process.memoryUsage().rss,
+  heap: process.memoryUsage().heapUsed,
 });
 
 const diff = (start: Snapshot, end: Snapshot): Snapshot => ({
   time: end.time - start.time,
-  memory: end.memory - start.memory,
+  rss: end.rss - start.rss,
+  heap: end.heap - start.heap,
 });
 
 export async function benchmark<T>(name: string, task: Task<T>): Promise<T> {
+  gc?.();
   const start = snapshot();
   const result = await task();
   const end = snapshot();
   const stats = diff(start, end);
   console.log('Task:', name);
   console.log('Time spent:', (stats.time / 1000n).toString(), 'us');
-  console.log('Memory used:', stats.memory.toString(), 'B');
+  console.log('RSS memory:', stats.rss.toString(), 'B');
+  console.log('Heap memory:', stats.heap.toString(), 'B');
   console.log('Result:', result);
   return result;
 }

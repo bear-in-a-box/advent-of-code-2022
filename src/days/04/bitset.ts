eaa90399ab0b744ac34e1aa8b3f206da3bb346b9
Wojciech Tałałaj <wojtal.pl@gmail.com>
2022-12-04 11:30:36 +0100

    Day 04 star 1

---
+++ src/days/04/bitset.ts
@@ -0,0 +1,34 @@
+export class Bitset {
+  private value: bigint;
+
+  constructor(value: bigint = 0n) {
+    this.value = value;
+  }
+
+  set(bit: number) {
+    this.value |= 1n << BigInt(bit);
+    return this;
+  }
+
+  clear(bit: number) {
+    this.value &= 1n << BigInt(bit);
+    return this;
+  }
+
+  test(bit: number): boolean {
+    const bitValue = 1n << BigInt(bit);
+    return (this.value & bitValue) === bitValue;
+  }
+
+  and(other: Bitset): Bitset {
+    return new Bitset(this.value & other.value);
+  }
+
+  equals(other: Bitset): boolean {
+    return this.value === other.value;
+  }
+
+  toBigInt(): BigInt {
+    return this.value.valueOf();
+  }
+}
